50f6ad178fea540ad26c845e754fec2d52e4fad7
JRK <jessym.reziga@gmail.com>
2016-03-19 23:02:42 +0100

    add example

---
+++ examples/BasicExample/BasicExample.ino
@@ -0,0 +1,40 @@
+#include <Jink.h>
+
+const int led = 13;
+
+void draw_text(char* buf, int x, int y)
+{
+	Jink.setColor(BLACK, WHITE);
+	Jink.clear();
+	Jink.setEnglishFont(ASCII64);
+	Jink.dispString(buf, x, y);
+	Jink.udpate();
+	delay(3000);
+}
+
+void setup(void)
+{
+	pinMode(led, OUTPUT);
+	digitalWrite(led, LOW);
+	Jink.begin(115200,8,9); // Baudrate, RX, TX
+}
+
+void loop(void)
+{
+	// Draw some text into e-display screen
+	draw_text("Hello World !", 200,250);
+	draw_text("That's an awesome display !", 20,250);
+	
+	// Low energy consumption mode
+	Jink.enterStopmode();
+  
+	while (1){
+		// Infinite bliking
+		digitalWrite(13, HIGH);
+		delay(1000);
+		digitalWrite(13, LOW);
+		delay(1000);
+	}
+}
+
+
